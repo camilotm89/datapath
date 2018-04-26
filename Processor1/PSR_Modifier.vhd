e642a3bce91dcb4a57e358ea2fdef9e9ca2d4a39
Cris635 <33696702+Cris635@users.noreply.github.com>
2018-04-26 17:51:57 -0500

    Add files via upload

--- Processor1/PSR_Modifier.vhd
+++ Processor1/PSR_Modifier.vhd
@@ -16,11 +16,11 @@ begin
 		process(aluop, out_ppal, crs1, crs2)
 			begin
 				if (aluop = "010000" or aluop = "011000")then -- Addcc y AddXcc
-					nzvc(0) <= (crs1 and crs2) or (not(out_ppal(31)) and (crs1 or crs2));
+					nzvc(0) <= (crs1(31) and crs2(31)) or (not(out_ppal(31)) and (crs1(31) or crs2(31)));
 			
 			else
 				if (aluop = "010100" or aluop = "011100")then -- Subcc y SubXcc
-					nzvc(0) <= (((not crs1) and crs2) or (out_ppal(31) and ((not crs1) or crs2)));
+					nzvc(0) <= (((not crs1(31)) and crs2(31)) or (out_ppal(31) and ((not crs1(31)) or crs2(31))));
 				end if;
 				
 				end if;
