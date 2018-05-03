3a907ea7ea6b7a13791facd86146d4c96789fbef
Cris635 <33696702+Cris635@users.noreply.github.com>
2018-05-03 17:55:17 -0500

    Update unidadControl.vhd

--- Processor1/unidadControl.vhd
+++ Processor1/unidadControl.vhd
@@ -43,6 +43,18 @@ begin
 				--aluop <= "001100";
 				when "011100" => ------> SubXcc
 				aluop <= "001101";
+				when "010001" => ------> Andcc
+				aluop <= "001110";
+				when "010101" => ------> Andncc
+				aluop <= "001111";
+				when "010010" => ------> Orcc
+				aluop "010000";
+				when "010110" => ------> Orncc
+				aluop "010001";
+				when "010011" => ------> Xorcc
+				aluop "010010";
+				when "010111" => ------> Xnorcc
+				aluop "010011";
 				when "111100" => ------> Save
 				aluop <= "000000"; -- Modifica CWP
 				when "111101" => ------> Restore
