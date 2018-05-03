b7c23a9c7260a5647a203d5ca7dc5f64752e1cd8
Cris635 <33696702+Cris635@users.noreply.github.com>
2018-05-03 18:18:48 -0500

    Add files via upload

--- Processor1/PSR.vhd
+++ Processor1/PSR.vhd
@@ -5,8 +5,8 @@ entity PSR is
     Port ( clock : in STD_LOGIC;
 			  reset : in STD_LOGIC;
 			  nzvc : in  STD_LOGIC_VECTOR (3 downto 0);
-			  nCWP : in STD_LOGIC_VECTOR (4 downto 0);
-			  CWP : out STD_LOGIC_VECTOR (4 downto 0);
+			  nCWP : in STD_LOGIC_VECTOR (1 downto 0);
+			  CWP : out STD_LOGIC_VECTOR (1 downto 0);
 			  c : out  STD_LOGIC);
 end PSR;
 
@@ -19,16 +19,15 @@ begin
 		begin
 		if (rising_edge(clock))then
 			if (reset = '1')then
-				CWP <= "00000";
+				CWP <= "00";
 				c <= '0';
 			else
 				Registro(23 downto 20) <= nzvc;
-				Registro(4 downto 0) <= nCWP;
+				Registro(1 downto 0) <= nCWP;
 				c <= Registro(20);
-				nCWP <= Registro(4 downto 0);
+				CWP <= Registro(1 downto 0);
 			end if;
 		end if;
 		end process;
 
 end Behavioral;
-
--- Processor1/PSR_Modifier.vhd
+++ Processor1/PSR_Modifier.vhd
@@ -17,14 +17,19 @@ begin
 			begin
 				if (aluop = "010000" or aluop = "011000")then -- Addcc y AddXcc
 					nzvc(0) <= (crs1(31) and crs2(31)) or (not(out_ppal(31)) and (crs1(31) or crs2(31)));
+					nzvc(1) <= (crs1(31) and crs2(31) and (not out_ppal(31))) or (crs1(31) and (not crs2(31)) and out_ppal(31));
 			
-			else
-				if (aluop = "010100" or aluop = "011100")then -- Subcc y SubXcc
-					nzvc(0) <= (((not crs1(31)) and crs2(31)) or (out_ppal(31) and ((not crs1(31)) or crs2(31))));
 				end if;
 				
+				if (aluop = "010100" or aluop = "011100")then -- Subcc y SubXcc
+					nzvc(0) <= (((not crs1(31)) and crs2(31)) or (out_ppal(31) and ((not crs1(31)) or crs2(31))));
+					nzvc(1) <= ((not crs1(31) and crs2(31)) or (out_ppal(31) and (not crs1(31)) and crs2(31)));
+				end if;
+				
+				if (aluop = "010001" or aluop = "010101" or aluop = "010010" or aluop = "010110" or aluop = "010011" or aluop = "010111")then
+					nzvc(1) <= '0';
+					nzvc(0) <= '0';				
 				end if;
 		end process;
 
 end Behavioral;
-
