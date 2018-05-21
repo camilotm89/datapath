3d1714d06868e79c0ace17cf837285bf0e2326ab
ctrejos <camilotm89@gmail.com>
2018-05-21 15:24:36 -0500

    Update mux4_1.vhd

--- Processor3/mux4_1.vhd
+++ Processor3/mux4_1.vhd
@@ -1,19 +1,12 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity mux is
     Port ( a : in  STD_LOGIC_VECTOR (31 downto 0);
            b : in  STD_LOGIC_VECTOR (31 downto 0);
-           sel : in  STD_LOGIC;
+	   c : in  STD_LOGIC_VECTOR (31 downto 0);
+           d : in  STD_LOGIC_VECTOR (31 downto 0);
+           sel : in  STD_LOGIC_VECTOR (1 downto 0);
            out_mux : out  STD_LOGIC_VECTOR (31 downto 0));
 end mux;
 
@@ -22,10 +15,15 @@ architecture Behavioral of mux is
 begin
 	process(a,b,sel)
 	begin
-		if (sel = '0')then
+		if (sel = '00')then
 			out_mux <= a;
-		else
+		else if (sel= '01')then
 			out_mux <= b;
+		else if (sel= '10')then
+			out_mux <= c;
+		else if (sel= '01')then
+			out_mux <= d;
+				
 		end if;
 	end process;
 
