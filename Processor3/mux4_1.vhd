42dc09975284afa17fa1bf90f419616713e9523f
Cris635 <33696702+Cris635@users.noreply.github.com>
2018-05-24 15:23:41 -0500

    Update Mux4_1

--- Processor3/mux4_1.vhd
+++ Processor3/mux4_1.vhd
@@ -4,7 +4,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity mux4_1 is
     Port ( a : in  STD_LOGIC_VECTOR (31 downto 0);
            b : in  STD_LOGIC_VECTOR (31 downto 0);
-	   c : in  STD_LOGIC_VECTOR (31 downto 0);
+			  c : in  STD_LOGIC_VECTOR (31 downto 0);
            d : in  STD_LOGIC_VECTOR (31 downto 0);
            sel : in  STD_LOGIC_VECTOR (1 downto 0);
            out_mux : out  STD_LOGIC_VECTOR (31 downto 0));
@@ -13,16 +13,16 @@ end mux4_1;
 architecture Behavioral of mux4_1 is
 
 begin
-	process(a,b,sel)
+	process(a,b,c,d,sel)
 	begin
 		if (sel = "00")then
-			out_mux <= a;
+			out_mux <= d;
 			else if (sel= "01")then
-				out_mux <= b;
+				out_mux <= a;
 				else if (sel= "10")then
-					out_mux <= c;
-					else if (sel= "01")then
-						out_mux <= d;							
+					out_mux <= b;
+					else if (sel= "11")then
+						out_mux <= c;							
 					end if;
 				end if;
 			end if;
